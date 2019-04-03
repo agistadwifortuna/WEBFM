4d1623cefeb1012ead0995bba1bc90cf74a1347a
agistadwifortuna <fortunaagista@gmail.com>
2019-04-03 12:03:44 +0700

    Project Modul 2-4

---
+++ penjualan/resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+ <html> 
+ <head> 
+ <title>Laravel Agista</title>
+ </head> 
+ <body> <h1>Produk : {{ isset($produk) ? $produk : 'Produk Kosong' }}</h1> 
+ </body> 
+ </html>
---
+++ penjualan/resources/views/produk/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+ <html> 
+ <head>
+ <title>Laravel Agista</title> 
+ </head>
+	<body>
+		<h1>Produk : {{ $produk }}</h1> 
+	</body> 
+</html>
